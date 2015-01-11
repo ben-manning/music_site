3a39756736e62335d1a5e195d1fcd71b7a0a068d
Ben Manning <bmanning@me.com>
2015-01-11 12:47:51 -0800

    add new paths for the website

--- music/app/controllers/albums_controller.rb
+++ music/app/controllers/albums_controller.rb
@@ -1,8 +1,46 @@
 class AlbumsController < ApplicationController
+  
+  def index
+  	@albums = Album.all
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
   def new
+  	@album = Album.new
+  end
+
+  def edit
+  	@album = Album.find(params[:id])
   end
 
   def create
-    render plain: params[:album].inspect
+    @album = Album.new(album_params)
+
+    if @album.save
+      redirect_to @album
+    else
+      render 'new'
+    end
   end
+
+  def update
+  	@album = Album.find(params[:id])
+
+  	if @album.update(album_params)
+  	  redirect_to @album
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  end
+
+  private
+    def album_params
+      params.require(:album).permit(:title, :artist, :year)
+    end
 end
--- music/app/models/album.rb
+++ music/app/models/album.rb
@@ -1,2 +1,5 @@
 class Album < ActiveRecord::Base
+  validates :title,  presence: true
+  validates :artist, presence: true
+  validates :year,   presence: true
 end
